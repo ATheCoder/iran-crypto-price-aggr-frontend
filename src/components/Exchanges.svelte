f471a097d3dcf56b39eb49cd9c24cac25002421a
Arash Arbabi <arbabiarash0@gmail.com>
2023-05-23 18:47:19 +0330

    feat: new design with purple background

--- src/components/Exchanges.svelte
+++ src/components/Exchanges.svelte
@@ -27,7 +27,7 @@
     <thead>
       <tr class="crypto-header">
         <th class="header-left">Exchange</th>
-        <th class="header-right">Price</th>
+        <th class="header-right">Buy Price</th>
       </tr>
     </thead>
     <tbody>
@@ -42,72 +42,3 @@
     </tbody>
   </table>
 </div>
-
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap");
-
-  .table-container {
-    max-width: 600px;
-    margin: 0 auto;
-    overflow-x: auto;
-    font-family: "Roboto", sans-serif;
-  }
-  .crypto-table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  .crypto-table th,
-  .crypto-table td {
-    padding: 15px;
-    text-align: left;
-  }
-  .crypto-header {
-    background-color: #f4f4f4;
-  }
-  .header-left {
-    border-top-left-radius: 10px;
-  }
-  .header-right {
-    border-top-right-radius: 10px;
-  }
-  .crypto-row {
-    background-color: #ffffff;
-    border-radius: 10px;
-    margin-bottom: 15px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.05);
-    transition: background-color 0.3s ease;
-  }
-  .crypto-row:hover {
-    background-color: #f8f8f8;
-  }
-  .exchange-name {
-    font-weight: 500;
-  }
-
-  .flash-green {
-    animation: flash-green-animation 3s;
-  }
-  .flash-red {
-    animation: flash-red-animation 3s;
-  }
-  @keyframes flash-green-animation {
-    0% {
-      background-color: #d4edda;
-      color: #155724;
-    }
-    100% {
-      background-color: inherit;
-      color: inherit;
-    }
-  }
-  @keyframes flash-red-animation {
-    0% {
-      background-color: #f8d7da;
-      color: #721c24;
-    }
-    100% {
-      background-color: inherit;
-      color: inherit;
-    }
-  }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,4 @@
   import Exchanges from "../components/Exchanges.svelte";
 </script>
 
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p> -->
 <Exchanges />
