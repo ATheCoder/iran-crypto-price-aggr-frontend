3ffb47c8da6cdf56d954c3b4af0210d776eaec34
Arash Arbabi <arbabiarash0@gmail.com>
2023-05-25 16:56:58 +0330

    feat: enable sockets

--- src/stores/exchanges.ts
+++ src/stores/exchanges.ts
@@ -1,35 +1,106 @@
 import { derived, readable, writable } from "svelte/store";
 
 // You might want to update this URL to the endpoint of your API
-const API_URL =
-  process.env.NODE_ENV === "dev"
-    ? "http://localhost:8080"
-    : "http://api-athena.athecoder.com";
 
-const fetchExchangeData = async () => {
-  const response = await fetch(API_URL);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const data = await response.json();
-  return data;
+const createCustomStore = () => {
+  let subscribersCount = 0;
+  let socket;
+  const SOCKET_ADDRESS = "ws://localhost:8080/ws";
+
+  const { subscribe, set, update } = writable([]);
+
+  return {
+    subscribe(run, invalidate = () => {}) {
+      if (subscribersCount === 0) {
+        if (typeof window !== "undefined") {
+          socket = new WebSocket(SOCKET_ADDRESS);
+
+          socket.onopen = () => {
+            console.log("Connected!");
+          };
+
+          socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            set(data.sort((e1, e2) => e1.buy - e2.buy));
+          };
+        }
+      }
+
+      subscribersCount += 1;
+      const unsubscribe = subscribe(run, invalidate);
+
+      return () => {
+        subscribersCount -= 1;
+        unsubscribe();
+
+        if (subscribersCount === 0) {
+          if (typeof socket !== "undefined") {
+            socket.close();
+          }
+          console.log("Disconnected!");
+        }
+      };
+    },
+  };
 };
 
-export const exchanges = readable([], function (set) {
-  const interval = setInterval(async () => {
-    const newExchangeData = await fetchExchangeData();
-    set(newExchangeData.sort((e1, e2) => e1.buy - e2.buy));
-  }, 3000);
-
-  return () => {
-    clearInterval(interval);
-  };
-});
-
-export const highlights = writable({});
+export const exchanges = createCustomStore();
 
 let previousValueOfExchanges = {};
 
+export const highlights = derived(exchanges, ($exchanges) => {
+  if (Object.keys(previousValueOfExchanges).length < 1) {
+    previousValueOfExchanges = convertArrayToKeyValue($exchanges);
+    return $exchanges.reduce((acc: any, curr: any) => {
+      return {
+        ...acc,
+        [curr.name]: {
+          buy: "",
+          sell: "",
+        },
+      };
+    }, {});
+  }
+
+  const newHighlights = $exchanges.reduce((acc, exchange) => {
+    const name = exchange.name;
+    const previousValueOfExchange = previousValueOfExchanges[name];
+    if (!previousValueOfExchange) {
+      return {
+        ...acc,
+        [name]: {
+          buy: "",
+          sell: "",
+        },
+      };
+    }
+
+    const currentBuyPrice = exchange.buy;
+    const previousBuyPrice = previousValueOfExchange.buy;
+
+    const currentSellPrice = exchange.sell;
+    const previousSellPrice = previousValueOfExchange.sell;
+    return {
+      ...acc,
+      [name]: {
+        buy:
+          currentBuyPrice > previousBuyPrice
+            ? "flash-green"
+            : currentBuyPrice < previousBuyPrice
+            ? "flash-red"
+            : "",
+        sell:
+          currentSellPrice > previousSellPrice
+            ? "flash-green"
+            : currentSellPrice < previousSellPrice
+            ? "flash-red"
+            : "",
+      },
+    };
+  }, {});
+
+  return newHighlights;
+});
 const convertArrayToKeyValue = (arr) => {
   return arr.reduce((acc, curr) => {
     return {
@@ -41,57 +112,3 @@ const convertArrayToKeyValue = (arr) => {
     };
   }, {});
 };
-
-exchanges.subscribe(($exchanges) => {
-  if (Object.keys(previousValueOfExchanges).length < 1) {
-    highlights.set(
-      $exchanges.reduce((acc, curr) => {
-        return { ...acc, [curr.name]: { buy: "", sell: "" } };
-      }, {})
-    );
-    previousValueOfExchanges = convertArrayToKeyValue($exchanges);
-    return;
-  }
-
-  highlights.set(
-    $exchanges.reduce((acc, exchange) => {
-      const name = exchange.name;
-      const previousValueOfExchange = previousValueOfExchanges[name];
-
-      if (!previousValueOfExchange) {
-        return {
-          ...acc,
-          [name]: {
-            buy: "",
-            sell: "",
-          },
-        };
-      }
-
-      const currentBuyPrice = exchange.buy;
-      const previousBuyPrice = previousValueOfExchange.buy;
-
-      const currentSellPrice = exchange.sell;
-      const previousSellPrice = previousValueOfExchange.sell;
-
-      return {
-        ...acc,
-        [name]: {
-          buy:
-            currentBuyPrice > previousBuyPrice
-              ? "flash-green"
-              : currentBuyPrice < previousBuyPrice
-              ? "flash-red"
-              : "",
-          sell:
-            currentSellPrice > previousSellPrice
-              ? "flash-green"
-              : currentSellPrice < previousSellPrice
-              ? "flash-red"
-              : "",
-        },
-      };
-    }, {})
-  );
-  previousValueOfExchanges = convertArrayToKeyValue($exchanges);
-});
