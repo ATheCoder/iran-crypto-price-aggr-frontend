ee1b14745ac3d44676dafed0b150a524d04ac5d9
Arash Arbabi <arbabiarash0@gmail.com>
2023-05-27 15:47:33 +0330

    feat: add reconnection logic for websockets

--- src/stores/exchanges.ts
+++ src/stores/exchanges.ts
@@ -6,24 +6,47 @@ import { derived, readable, writable } from "svelte/store";
 const createCustomStore = () => {
   let subscribersCount = 0;
   let socket;
+  let reconnectAttempt = 0;
   const SOCKET_ADDRESS = "ws://api-athena.athecoder.com/ws";
 
   const { subscribe, set, update } = writable([]);
+
+  function connect() {
+    socket = new WebSocket(SOCKET_ADDRESS);
+
+    socket.onopen = () => {
+      console.log("Connected!");
+      reconnectAttempt = 0; // reset the reconnect attempt counter
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      set(data.sort((e1, e2) => e1.buy - e2.buy));
+    };
+
+    socket.onclose = (event) => {
+      console.log("WebSocket connection closed: ", event);
+      if (!event.wasClean) {
+        // Connection not cleanly closed. Attempt to reconnect.
+        const timeout = Math.min(1000 * 2 ** reconnectAttempt, 30000); // Exponential backoff capped at 30 seconds
+        console.log(
+          `WebSocket connection lost. Reconnecting in ${timeout}ms...`
+        );
+        setTimeout(connect, timeout);
+        reconnectAttempt += 1;
+      }
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error observed: ", error);
+    };
+  }
 
   return {
     subscribe(run, invalidate = () => {}) {
       if (subscribersCount === 0) {
         if (typeof window !== "undefined") {
-          socket = new WebSocket(SOCKET_ADDRESS);
-
-          socket.onopen = () => {
-            console.log("Connected!");
-          };
-
-          socket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            set(data.sort((e1, e2) => e1.buy - e2.buy));
-          };
+          connect();
         }
       }
 
