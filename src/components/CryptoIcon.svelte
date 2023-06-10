38a72d0e8124a3fe1ed0bbc294bbfca38c1fdf91
Arash Arbabi <arbabiarash0@gmail.com>
2023-06-10 15:37:24 +0330

    feat: add tether icon on the left side

---
+++ src/components/CryptoIcon.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let imgSrc = "";
+  export let altText = "";
+  export let onClick = () => {};
+</script>
+
+<img class="crypto-icon" src={imgSrc} {altText} on:click={onClick} />
+
+<style>
+  .crypto-icon {
+    cursor: pointer;
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin: 2px;
+    transition: transform 0.3s ease;
+  }
+
+  .crypto-icon:hover {
+    transform: scale(1.1);
+  }
+</style>
--- src/components/Exchanges.svelte
+++ src/components/Exchanges.svelte
@@ -1,9 +1,16 @@
 <script>
   import { exchanges, highlights } from "../stores/exchanges";
   import { flip } from "svelte/animate";
+  import CryptoIcon from "./CryptoIcon.svelte";
 </script>
 
 <div class="table-container">
+  <div style="position: relative;">
+    <div class="small-rectangle">
+      <CryptoIcon imgSrc="tether.png" />
+      <!-- <CryptoIcon imgSrc="bitcoin.png" /> -->
+    </div>
+  </div>
   <table class="crypto-table">
     <thead>
       <tr class="crypto-header">
@@ -23,3 +30,24 @@
     </tbody>
   </table>
 </div>
+
+<style>
+  .small-rectangle {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    position: absolute;
+    padding: 2rem;
+    left: -5.5rem;
+    width: 2.5rem;
+    padding: 10px;
+    /* height: 100px; */
+    border-radius: 6px 0px 0px 6px;
+    background-color: white;
+    /* transform: translate(-100%, -50%); */
+    box-shadow: 10px px 25px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease-in-out;
+    z-index: 9;
+  }
+</style>
